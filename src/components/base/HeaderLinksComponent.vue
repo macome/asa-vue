69fec8ed94fc71465858600e69d505cb3139fa8c
aly <alymacome@gmail.com>
2020-07-12 12:31:44 +0200

    Adicao dos ficheiros do template & criacao do HeaderLinkComponent

---
+++ src/components/base/HeaderLinksComponent.vue
@@ -0,0 +1,89 @@
+<template>
+    <section>
+     <div class="bg-top navbar-light d-flex flex-column-reverse">
+    	<div class="container py-3">
+    		<div class="row no-gutters d-flex align-items-center align-items-stretch">
+    			<div class="col-md-4 d-flex align-items-center py-4">
+    				<a class="navbar-brand" href="index.html">Negotiate <span>Consulting Agency</span></a>
+    			</div>
+	    		<div class="col-lg-8 d-block">
+		    		<div class="row d-flex">
+					    <div class="col-md d-flex topper align-items-center align-items-stretch py-md-4">
+					    	<div class="icon d-flex justify-content-center align-items-center"><span class="ion-ios-paper-plane"></span></div>
+					    	<div class="text">
+					    		<span>Email</span>
+						    	<span>youremail@email.com</span>
+						    </div>
+					    </div>
+					    <div class="col-md d-flex topper align-items-center align-items-stretch py-md-4">
+					    	<div class="icon d-flex justify-content-center align-items-center"><span class="ion-ios-call"></span></div>
+						    <div class="text">
+						    	<span>Call</span>
+						    	<span>+123 523 5598</span>
+						    </div>
+					    </div>
+					    <div class="col-md d-flex topper align-items-center align-items-stretch py-md-4">
+					    	<div class="icon d-flex justify-content-center align-items-center"><span class="ion-ios-time"></span></div>
+						    <div class="text">
+						    	<span>Working Hours</span>
+						    	<span>Mon - Sat 8am - 5pm</span>
+						    </div>
+					    </div>
+				    </div>
+			    </div>
+		    </div>
+		  </div>
+		  <div class="top-social-menu py-2 bg-light">
+		  	<div class="container">
+		  		<div class="row">
+			    	<div class="col">
+			    		<p class="social mb-0">
+			    			<a href="#"><i class="ion-logo-facebook"></i><span class="sr-only">Facebook</span></a>
+			    			<a href="#"><i class="ion-logo-twitter"></i><span class="sr-only">Twitter</span></a>
+			    			<a href="#"><i class="ion-logo-googleplus"></i><span class="sr-only">Googleplus</span></a>
+			    		</p>
+			    	</div>
+			    	<div class="col text-right">
+			    		<a href="#" class="btn-link">Request A Quote</a>
+			    	</div>
+			    </div>
+		  	</div>
+		  </div>
+    </div>
+     <nav class="navbar navbar-expand-lg navbar-dark bg-dark ftco-navbar-light" id="ftco-navbar">
+	    <div class="container d-flex align-items-center">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="oi oi-menu"></span> Menu
+	      </button>
+	      <form action="#" class="searchform order-lg-last">
+          <div class="form-group d-flex">
+            <input type="text" class="form-control pl-3" placeholder="Search">
+            <button type="submit" placeholder="" class="form-control search"><span class="ion-ios-search"></span></button>
+          </div>
+        </form>
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav mr-auto">
+	        	<li class="nav-item"><a href="index.html" class="nav-link">Home</a></li>
+	        	<li class="nav-item active"><a href="about.html" class="nav-link">About</a></li>
+	        	<li class="nav-item"><a href="team.html" class="nav-link">Team</a></li>
+	        	<li class="nav-item"><a href="project.html" class="nav-link">Case Studies</a></li>
+	        	<li class="nav-item"><a href="services.html" class="nav-link">Services</a></li>
+	        	<li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
+	          <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
+    <!-- END nav -->
+    </section>
+</template>
+
+<script>
+    export default {
+        name:"HeaderLinksComponent"
+    }
+</script>
+
+<style scoped>
+
+</style>
